cba3fdbd24921302f1fbafeaed1a5d52c2f6d4fa
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-03 10:55:28 +0800

    fix app.jsx and index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,14 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-const App = () => {
+export const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
       <ContactForm />
-
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
     </div>
   );
 };
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './PhonebookRedux/store'; // Adjust the import path as necessary
-import App from './components/App';
+import { store, persistor } from './PhonebookRedux/store';
+import { App } from './components/App'; // Corrected import statement
 import './index.css';
 
 ReactDOM.render(
